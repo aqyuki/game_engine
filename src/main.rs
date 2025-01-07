3a0bcf92f88b205abc1a3186677cd834a5e0d7f9
aqyuki <aqyuki@aqyuki.dev>
2025-01-07 22:02:45 +0900

    ✨ エラーを適切に処理するようにした

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ const APPLICATION_NAME: &str = "Original Game Engine";
 
 fn main() {
     let engine = engine::engine::Engine::new(APPLICATION_NAME);
-    engine.start();
+    match engine.start() {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
